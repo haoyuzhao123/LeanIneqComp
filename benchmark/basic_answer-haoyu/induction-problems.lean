36bc875488313fa298b4199f89501f70f8652e7d
haoyuzhao123 <thomaszhao1998@gmail.com>
2025-03-12 01:13:55 -0400

    add some problems that requires induction

---
+++ benchmark/basic_answer-haoyu/induction-problems.lean
@@ -0,0 +1,78 @@
+import Mathlib
+import Aesop
+
+set_option maxHeartbeats 0
+open BigOperators Real Nat Topology Rat
+
+theorem induction_p1 (n : ℕ) (h : n ≥ 4) : 2 ^ n ≥ n + 1 := by
+  induction' h with k h IH
+  · calc 2 ^ 4 = 16 := by norm_num
+      _ ≥ 4 + 1 := by norm_num
+  calc 2 ^ k.succ = 2 ^ k * 2 := Nat.pow_succ 2 k
+    _ ≥ (k + 1) * 2 := by linarith
+    _ = 2 * k + 2 := by ring
+    _ ≥ k + 2 := by linarith
+
+theorem induction_p2_minif2f_induction_1pxpownlt1pnx (x : ℝ) (n : ℕ) (h₀ : -1 < x) (h₁ : 0 < n) : 1 + ↑n * x ≤ (1 + x) ^ (n : ℕ) := by
+  induction' h₁ with k h₁ IH
+  · calc 1 + ↑(succ 0) * x = 1 + x := by simp
+      _ ≤ (1 + x) ^ (succ 0 : ℕ) := by simp
+  calc 1 + ↑k.succ * x = 1 + ↑k * x + x := by rw [Nat.cast_succ] ; ring
+    _ ≤ 1 + ↑k * x + x + ↑k * x * x := by nlinarith [ sq_nonneg (x) ]
+    _ = (1 + x) * (1 + ↑k * x) := by ring
+    _ ≤ (1 + x) * (1 + x) ^ (k : ℕ) := by nlinarith
+    _ = (1 + x) ^ k.succ := by rw [pow_succ]; ring
+
+theorem induction_p3_minif2f_induction_ineq_nsqlefactn (n : ℕ) (h₀ : 4 ≤ n) : n ^ 2 ≤ n ! := by
+  induction' h₀ with k g IH
+  · calc 4 ^ 2 = 16 := by simp
+      _ ≤ 24 := by simp
+      _ = 4 ! := by simp [Nat.factorial_succ]
+  have j : k ≥ 4 := by norm_cast
+  calc k.succ ^ 2 = (k + 1) ^ 2 := by simp
+    _ = k ^ 2 + 2 * k + 1 := by ring
+    _ ≤ k ^ 2 + 2 * k + 4 := by simp
+    _ ≤ k ^ 2 + 2 * k + k := by simp_all
+    _ = k ^ 2 + 3 * k := by ring
+    _ ≤ k ^ 2 + k ^ 2 := by nlinarith
+    _ = 2 * k ^ 2 := by ring
+    _ ≤ k.succ * k ^ 2 := by nlinarith
+    _ ≤ k.succ * k ! := by nlinarith
+    _ = k.succ ! := by apply Nat.factorial_succ
+
+example (n : ℕ) (h₀ : 3 ≤ n) : n.succ ^ (n - 1) ≥ n ^ (n - 1) := by
+  -- Apply the lemma that if `a ≤ b`, then `a^k ≤ b^k` for non-negative `k`.
+  apply Nat.pow_le_pow_of_le_left
+  -- Prove that `n ≤ n.succ` (since `n.succ = n + 1`).
+  exact Nat.le_succ n
+
+theorem induction_p4_minif2f_induction_nfactltnexpnm1ngt3 (n : ℕ) (h₀ : 3 ≤ n) : n ! < n ^ (n - 1) := by
+  induction' h₀ with k g IH
+  · calc 3 ! = 6 := by simp [Nat.factorial_succ]
+      _ < 9 := by simp
+      _ = 3 ^ (3 - 1) := by simp
+
+  have lk : k ^ (k - 1) ≤ k.succ ^ (k - 1) := by
+    -- Apply the lemma that if `a ≤ b`, then `a^j ≤ b^j` for non-negative `j`.
+    apply Nat.pow_le_pow_of_le_left
+    -- Prove that `k ≤ k.succ` (since `k.succ = k + 1`).
+    exact Nat.le_succ k
+
+  -- Haoyu: Copied from STP and Goedel prover, purely magic, don't understand why it pass the compilation
+  have lksucc : k.succ * ( k.succ ^ (k - 1) ) = k.succ ^ k := by
+    induction' g with j h hk
+    <;> simp_all [Nat.pow_succ]
+    <;> ring
+
+  calc k.succ ! = k.succ * k ! := by apply Nat.factorial_succ
+    _ < k.succ * k ^ (k - 1) := by nlinarith
+    -- Apply the lemma that if `a ≤ b`, then `a^k ≤ b^k` for non-negative `k`.
+    -- Prove that `n ≤ n.succ` (since `n.succ = n + 1`).
+    _ ≤ k.succ * (k.succ ^ (k - 1)) := by nlinarith
+    --_ = k.succ ^ (k - 1) * k.succ := by ring
+    _ = k.succ ^ k := by apply lksucc
+
+
+theorem induction_p5_minif2f_induction_pord1p1on2powklt5on2 (n : ℕ) (h₀ : 0 < n) : (∏ k in Finset.Icc 1 n, 1 + (1 : ℝ) / 2 ^ k) < 5 / 2 := by sorry
+
+theorem induction_p6_minif2f_induction_prod1p1onk3le3m1onn (n : ℕ) (h₀ : 0 < n) : (∏ k in Finset.Icc 1 n, 1 + (1 : ℝ) / k ^ 3) ≤ (3 : ℝ) - 1 / ↑n := by
