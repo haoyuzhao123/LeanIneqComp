c4582aab4c809e3d59843f51ad5e70ea2be110c8
haoyuzhao123 <thomaszhao1998@gmail.com>
2025-03-16 23:24:15 -0400

    finish amgm validation problems

--- benchmark/basic_answer-haoyu/amgm-problems.lean
+++ benchmark/basic_answer-haoyu/amgm-problems.lean
@@ -404,6 +404,7 @@ theorem amgm_p12 (x y z: ℝ ) (h₁: x+ 2 * y + 2 * z = 10) (h₂ : x > 0 ∧ y
     _ ≤ 2 ^ 5 := xyyzzonefifth'
     _ = 32 := by norm_num
 
+
 theorem amgm_p13 (x y: ℝ )  (h : x > 0 ∧ y> 0): (2:ℝ) / 3 * x ^ 3 + (1:ℝ) / 3 * y ^ 3  ≥ x^2 * y := by
   -- Step 1: Define the three numbers to apply AM-GM
   let S := ![x^3, y^3]
@@ -457,7 +458,63 @@ theorem amgm_p13 (x y: ℝ )  (h : x > 0 ∧ y> 0): (2:ℝ) / 3 * x ^ 3 + (1:ℝ
   calc (2:ℝ) / 3 * x ^ 3 + (1:ℝ) / 3 * y ^ 3 ≥ (x ^ 3) ^ ((2:ℝ)/3) * (y ^ 3 )^ (3⁻¹: ℝ ) := by nlinarith
     _ = x ^ 2 * y := by nlinarith
 
-theorem amgm_p14 (x y: ℝ )  (h : x > 0 ∧ y> 0): (4:ℝ) / 7 * x ^ 7 + (3:ℝ) / 7 * y ^ 7  ≥ x^4 * y^3 := by
+theorem amgm_p14 (x y: ℝ )  (h : x > 0 ∧ y> 0): (2:ℝ) / 3 * x ^ 6 + (1:ℝ) / 3 * y ^ 6  ≥ x^4 * y^2 := by
+  -- Step 1: Define the three numbers to apply AM-GM
+  let S := ![x^6, y^6]
+  let l := ![(2:ℝ) / 3, (1:ℝ) / 3]
+
+  have x2p : 0 < x ^ 2 := by nlinarith
+  have x3p : 0 < x ^ 3 := by nlinarith
+  have x6p : 0 < x ^ 6 := by nlinarith
+  have y2p : 0 < y ^ 2 := by nlinarith
+  have y3p : 0 < y ^ 3 := by nlinarith
+  have y6p : 0 < y ^ 6 := by nlinarith
+
+  have h_nonneg : ∀ i ∈ Finset.univ, 0 ≤ S i := by
+      intros i
+      fin_cases i
+      all_goals
+        simp [S]
+        linarith
+
+  have l_nonneg : ∀ i ∈ Finset.univ, 0 ≤ l i := by
+      intros i
+      fin_cases i
+      all_goals
+        simp [l]
+        <;> norm_num
+
+  have l_sump : 0 < ∑ i : Fin 2, l i := by
+      simp [l]
+      norm_num
+
+  -- Apply AM-GM inequality
+  have amgm : (∏ i : Fin 2, S i ^ (l i : ℝ)) ^ ((∑ i : Fin 2, (l i : ℝ))⁻¹) ≤ (∑ i : Fin 2, (l i : ℝ) * S i) / (∑ i: Fin 2, (l i : ℝ)) := by
+    apply Real.geom_mean_le_arith_mean
+    exact l_nonneg
+    exact l_sump
+    exact h_nonneg
+
+  simp [S] at amgm
+  simp [l] at amgm
+  norm_num at amgm
+  simp [Fin.prod_univ_two] at amgm
+
+  have xtrans : (x ^ 6) ^ ((2:ℝ)/3) = x ^ 4 := by
+    rw [← Real.rpow_natCast]
+    rw [← Real.rpow_mul (le_of_lt h.1) ]
+    norm_num
+    norm_cast
+
+  have ytrans : (y ^ 6 )^ (3⁻¹: ℝ ) = y ^ 2 := by
+    rw [← Real.rpow_natCast]
+    rw [← Real.rpow_mul (le_of_lt h.2) ]
+    norm_num
+
+  calc (2:ℝ) / 3 * x ^ 6 + (1:ℝ) / 3 * y ^ 6 ≥ (x ^ 6) ^ ((2:ℝ)/3) * (y ^ 6 )^ (3⁻¹: ℝ ) := by nlinarith
+    _ = x ^ 4 * y^2 := by rw [xtrans, ytrans]
+
+theorem amgm_p15 (x y: ℝ )  (h : x > 0 ∧ y> 0): (4:ℝ) / 7 * x ^ 7 + (3:ℝ) / 7 * y ^ 7  ≥ x^4 * y^3 := by
   -- Step 1: Define the three numbers to apply AM-GM
   let S := ![x^7, y^7]
   let l := ![(4:ℝ) / 7, (3:ℝ) / 7]
@@ -806,7 +863,7 @@ theorem amgm_p20 (x y z w: ℝ) (hx : x > 0) (hy : y > 0) (hz : z > 0) (hw : w >
     rw [← Real.rpow_natCast]
     rw [← Real.rpow_mul (le_of_lt hz)]
     norm_num
-  
+
   have wtrans : (w ^ 6 )^ (6⁻¹: ℝ ) = w := by
     rw [← Real.rpow_natCast]
     rw [← Real.rpow_mul (le_of_lt hw)]
@@ -842,7 +899,7 @@ theorem amgm_p21 (x y z: ℝ) (hx : x > 0) (hy : y > 0) (hz : z > 0) : (2:ℝ)/3
       simp [l]
       simp [Fin.sum_univ_three]
       norm_num
-  
+
   have amgm : (∏ i : Fin 3, S i ^ (l i : ℝ)) ^ ((∑ i : Fin 3, (l i : ℝ))⁻¹) ≤ (∑ i : Fin 3, (l i : ℝ) * S i) / (∑ i: Fin 3, (l i : ℝ)) := by
       apply Real.geom_mean_le_arith_mean
       exact l_nonneg
@@ -869,7 +926,7 @@ theorem amgm_p21 (x y z: ℝ) (hx : x > 0) (hy : y > 0) (hz : z > 0) : (2:ℝ)/3
     rw [← Real.rpow_natCast]
     rw [← Real.rpow_mul (le_of_lt hz) ]
     norm_num
-  
+
   calc (2:ℝ)/3 * x^2 + (1:ℝ)/6 * y^2 + (1:ℝ)/6 * z^2 ≥ (x ^ 2) ^ ((2: ℝ)/3 ) * (y ^ 2 )^ ((1: ℝ)/6 ) * (z ^ 2 )^ ((1: ℝ)/6 ) := by nlinarith
     _ = x ^ ((4: ℝ)/3 ) * y ^ ((1: ℝ)/3 ) * z ^ ((1: ℝ)/3 ) := by rw [xtrans, ytrans, ztrans]
 
@@ -1121,7 +1178,7 @@ theorem amgm_p24 (a b c : ℝ) (ap : a > 0) (bp : b> 0) (cp : c> 0) : a^3 + b^3 
 
     calc (2:ℝ) / 3 * x ^ 3 + (1:ℝ) / 3 * y ^ 3 ≥ (x ^ 3) ^ ((2:ℝ)/3) * (y ^ 3 )^ (3⁻¹: ℝ ) := by nlinarith
       _ = x ^ 2 * y := by nlinarith
-  
+
   have hab : (2:ℝ) / 3 * a^3 + (1:ℝ) / 3 * b^3  ≥ a^2 * b := by apply x3y3gex2ylem a b ap bp
   have hbc : (2:ℝ) / 3 * b^3 + (1:ℝ) / 3 * c^3  ≥ b^2 * c := by apply x3y3gex2ylem b c bp cp
   have hca : (2:ℝ) / 3 * c^3 + (1:ℝ) / 3 * a^3  ≥ c^2 * a := by apply x3y3gex2ylem c a cp ap
@@ -1186,7 +1243,7 @@ theorem amgm_p25 (a b c : ℝ) (ap : a > 0) (bp : b> 0) (cp : c> 0) : a^7 + b^7 
 
     calc (4:ℝ) / 7 * x ^ 7 + (3:ℝ) / 7 * y ^ 7 ≥ (x ^ 7) ^ ((4:ℝ)/7) * (y ^ 7 )^ ((3:ℝ)/7) := by nlinarith
       _ = x ^ 4 * y ^ 3 := by rw [xtrans, ytrans]
-  
+
   have hab : (4:ℝ)/7 * a^7 + (3:ℝ)/7 * b^7  ≥ a^4 * b^3 := by apply x7y7gex4y3lem a b ap bp
   have hbc : (4:ℝ)/7 * b^7 + (3:ℝ)/7 * c^7  ≥ b^4 * c^3 := by apply x7y7gex4y3lem b c bp cp
   have hca : (4:ℝ)/7 * c^7 + (3:ℝ)/7 * a^7  ≥ c^4 * a^3 := by apply x7y7gex4y3lem c a cp ap
