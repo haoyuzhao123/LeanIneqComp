78e8d81076e3a1ee2d8b058145b65517b9bbc237
haoyuzhao123 <thomaszhao1998@gmail.com>
2025-03-16 23:51:04 -0400

    add amgm valid jsonl file

--- benchmark/basic_answer-haoyu/amgm-problems.lean
+++ benchmark/basic_answer-haoyu/amgm-problems.lean
@@ -405,15 +405,17 @@ theorem amgm_p12 (x y z: ℝ ) (h₁: x+ 2 * y + 2 * z = 10) (h₂ : x > 0 ∧ y
     _ = 32 := by norm_num
 
 
-theorem amgm_p13 (x y: ℝ )  (h : x > 0 ∧ y> 0): (2:ℝ) / 3 * x ^ 3 + (1:ℝ) / 3 * y ^ 3  ≥ x^2 * y := by
+theorem amgm_p13 (x y: ℝ )  (h : x > 0 ∧ y> 0): (4:ℝ) / 5 * x ^ 5 + (1:ℝ) / 5 * y ^ 5  ≥ x^4 * y := by
   -- Step 1: Define the three numbers to apply AM-GM
-  let S := ![x^3, y^3]
-  let l := ![(2:ℝ) / 3, (1:ℝ) / 3]
+  let S := ![x^5, y^5]
+  let l := ![(4:ℝ) / 5, (1:ℝ) / 5]
 
   have x2p : 0 < x ^ 2 := by nlinarith
   have x3p : 0 < x ^ 3 := by nlinarith
+  have x5p : 0 < x ^ 5 := by nlinarith
   have y2p : 0 < y ^ 2 := by nlinarith
   have y3p : 0 < y ^ 3 := by nlinarith
+  have y5p : 0 < y ^ 5 := by nlinarith
 
   have h_nonneg : ∀ i ∈ Finset.univ, 0 ≤ S i := by
       intros i
@@ -445,18 +447,19 @@ theorem amgm_p13 (x y: ℝ )  (h : x > 0 ∧ y> 0): (2:ℝ) / 3 * x ^ 3 + (1:ℝ
   norm_num at amgm
   simp [Fin.prod_univ_two] at amgm
 
-  have xtrans : (x ^ 3) ^ ((2:ℝ)/3) = x ^ 2 := by
+  have xtrans : (x ^ 5) ^ ((4:ℝ)/5) = x ^ 4 := by
     rw [← Real.rpow_natCast]
     rw [← Real.rpow_mul (le_of_lt h.1) ]
     norm_num
+    norm_cast
 
-  have ytrans : (y ^ 3 )^ (3⁻¹: ℝ ) = y := by
+  have ytrans : (y ^ 5 )^ (5⁻¹: ℝ ) = y := by
     rw [← Real.rpow_natCast]
     rw [← Real.rpow_mul (le_of_lt h.2) ]
     norm_num
 
-  calc (2:ℝ) / 3 * x ^ 3 + (1:ℝ) / 3 * y ^ 3 ≥ (x ^ 3) ^ ((2:ℝ)/3) * (y ^ 3 )^ (3⁻¹: ℝ ) := by nlinarith
-    _ = x ^ 2 * y := by nlinarith
+  calc (4:ℝ) / 5 * x ^ 5 + (1:ℝ) / 5 * y ^ 5 ≥ (x ^ 5) ^ ((4:ℝ)/5) * (y ^ 5 )^ (5⁻¹: ℝ ) := by nlinarith
+    _ = x ^ 4 * y := by rw [xtrans, ytrans]
 
 theorem amgm_p14 (x y: ℝ )  (h : x > 0 ∧ y> 0): (2:ℝ) / 3 * x ^ 6 + (1:ℝ) / 3 * y ^ 6  ≥ x^4 * y^2 := by
   -- Step 1: Define the three numbers to apply AM-GM
@@ -872,7 +875,7 @@ theorem amgm_p20 (x y z w: ℝ) (hx : x > 0) (hy : y > 0) (hz : z > 0) (hw : w >
   calc (1:ℝ)/3 * x^6 + (1:ℝ)/3 * y^6 + (1:ℝ)/6 * z^6 + (1:ℝ)/6 * w^6 ≥ (x ^ 6) ^ ((1:ℝ)/3) * (y ^ 6) ^ ((1:ℝ)/3) * (z ^ 6) ^ (6⁻¹:ℝ) * (w ^ 6) ^ (6⁻¹:ℝ) := by nlinarith
     _ = x^2 * y^2 * z * w := by rw [xtrans, ytrans, ztrans, wtrans]
 
-theorem amgm_p21 (x y z: ℝ) (hx : x > 0) (hy : y > 0) (hz : z > 0) : (2:ℝ)/3 * x^2 + (1:ℝ)/6 * y^2 + (1:ℝ)/6 * z^2 ≥ x^((4:ℝ)/3) * y^((1:ℝ)/3) * z^((1:ℝ)/3):= by
+theorem amgm_p21 (x y z: ℝ) (hx : x > 0) (hy : y > 0) (hz : z > 0) : (2:ℝ)/3 * x^2 + (1:ℝ)/6 * y^2 + (1:ℝ)/6 * z^2 ≥ x^((4:ℝ)/3) * y^((1:ℝ)/3) * z^((1:ℝ)/3) := by
   -- Step 1: Define the three numbers to apply AM-GM
   let S := ![x^2, y^2, z^2]
   let l := ![(2:ℝ)/3, (1:ℝ)/6, (1:ℝ)/6]
