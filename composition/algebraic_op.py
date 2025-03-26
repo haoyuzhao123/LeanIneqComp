f43406d68ee99b04d5457518f3cf0f62b3b929b1
haoyuzhao123 <thomaszhao1998@gmail.com>
2025-03-26 14:13:09 -0400

    add more algebraic transformations

--- composition/algebraic_op.py
+++ composition/algebraic_op.py
@@ -1,12 +1,19 @@
 import copy
 import random
+import string
 
 from problem import IneqProblem
 
-def algebraic_op(P1, mode="sqrt_all"):
+def algebraic_op(P1, mode="reset_from_a", additional_arg=None):
     variables = P1.variables
-    
-    if mode == "sqrt_all":
+    new_variable_list = None
+    if mode == "reset_from_a":
+        transform_dict, new_variable_list = _reset_from_a(P1)
+    elif mode == "shift_var":
+        transform_dict, new_variable_list = _shift_var(P1)
+    elif mode == "mod_var_idx":
+        transform_dict, new_variable_list = _mod_var_idx(P1, additional_arg)
+    elif mode == "sqrt_all":
         transform_dict = _sqrt_all(P1)
     elif mode == "sqrt_random":
         transform_dict = _sqrt_random(P1)
@@ -22,11 +29,22 @@ def algebraic_op(P1, mode="sqrt_all"):
         transform_dict = _reciprocal_all(P1)
     elif mode == "reciprocal_random":
         transform_dict = _reciprocal_random(P1)
+    elif mode == "exp_all":
+        transform_dict = _exp_all(P1)
+    elif mode == "exp_random":
+        transform_dict = _exp_random(P1)
+    elif mode == "log_all":
+        transform_dict = _log_all(P1)
+    elif mode == "log_random":
+        transform_dict = _log_random(P1)
     else:
         raise ValueError("Not implemented algebraic transformation!")
     
     P = IneqProblem()
-    new_variables = copy.deepcopy(variables)
+    if new_variable_list != None:
+        new_variables = new_variable_list
+    else:
+        new_variables = copy.deepcopy(variables)
     P.set_variables(new_variables)
 
     if P1.condition != None:
@@ -40,8 +58,68 @@ def algebraic_op(P1, mode="sqrt_all"):
 
     return P
 
-def shift_var(P1):
-    pass
+def _reset_from_a(P1):
+    # reset all variables starting from a
+    # eg, previously has x y z
+    # reset to a b c
+    variables = P1.variables
+    total_num = len(variables)
+    assert total_num <= 26 , "Problem has too many (>26) variables!"
+    transform_dict = dict()
+    for i, v in enumerate(variables):
+        str_temp = "{"+string.ascii_lowercase[i]+"}"
+        transform_dict[v] = str_temp
+    
+    new_variable_list = []
+    for i in range(total_num):
+        new_variable_list.append(string.ascii_lowercase[i])
+    
+    return transform_dict, new_variable_list
+
+def _shift_var(P1):
+    # shift by n
+    # e.g., previous a b c, n=2
+    # change to c d e
+    variables = P1.variables
+    total_num = len(variables)
+    assert total_num <= 26 , "Problem has too many (>26) variables!"
+
+    char_to_index = dict(zip(string.ascii_lowercase, range(26)))
+
+    transform_dict = dict()
+    new_variable_list = []
+    for v in variables:
+        i = char_to_index[v]
+        # make sure the variables lies in a-z
+        i = (i+1) % 26
+        str_temp = "{"+string.ascii_lowercase[i]+"}"
+        transform_dict[v] = str_temp
+        new_variable_list.append(string.ascii_lowercase[i])
+    
+    return transform_dict, new_variable_list
+
+def _mod_var_idx(P1, n):
+    # shift by n
+    # e.g., previous a b c, n=2
+    # change to c d e
+    variables = P1.variables
+    total_num = len(variables)
+    assert total_num <= 26 , "Problem has too many (>26) variables!"
+
+    char_to_index = dict(zip(string.ascii_lowercase, range(26)))
+
+    transform_dict = dict()
+    new_variable_list = []
+    for v in variables:
+        i = char_to_index[v]
+        # make sure the variables lies in a-z
+        i = i % n
+        str_temp = "{"+string.ascii_lowercase[i]+"}"
+        transform_dict[v] = str_temp
+        assert string.ascii_lowercase[i] not in new_variable_list , "Mod operation failed : two variables mapped to same after mod."
+        new_variable_list.append(string.ascii_lowercase[i])
+    
+    return transform_dict, new_variable_list
 
 def _sqrt_all(P1):
     variables = P1.variables
@@ -127,14 +205,58 @@ def _reciprocal_random(P1):
     str_temp = "(1/{"+vr+"})"
     transform_dict[vr] = str_temp
 
+def _exp_all(P1):
+    variables = P1.variables
+    transform_dict = dict()
+    for v in variables:
+        str_temp = "((Real.exp {"+v+"}) - 1)"
+        transform_dict[v] = str_temp
+    
+    return transform_dict
+
+def _exp_random(P1):
+    variables = P1.variables
+    transform_dict = dict()
+    for v in variables:
+        str_temp = "{"+v+"}"
+        transform_dict[v] = str_temp
+
+    vr = random.choice(variables)
+    str_temp = "((Real.exp {"+v+"}) - 1)"
+    transform_dict[vr] = str_temp
+
+def _log_all(P1):
+    variables = P1.variables
+    transform_dict = dict()
+    for v in variables:
+        str_temp = "(Real.log ({"+v+"}+1))"
+        transform_dict[v] = str_temp
+    
+    return transform_dict
+
+def _log_random(P1):
+    variables = P1.variables
+    transform_dict = dict()
+    for v in variables:
+        str_temp = "{"+v+"}"
+        transform_dict[v] = str_temp
+
+    vr = random.choice(variables)
+    str_temp = "(Real.log ({"+vr+"}+1))"
+    transform_dict[vr] = str_temp
+
 if __name__ == '__main__':
     P1 = IneqProblem()
     P1.set_name("test")
-    P1.set_variables(["a", "b", "c"])
-    P1.set_condition("{a} + {b} + {c} = 1")
-    P1.set_statement_lhs("(1:ℝ)/{a} + (1:ℝ)/{b} + (1:ℝ)/{c}")
+    P1.set_variables(["x", "y", "z"])
+    P1.set_condition("{x} + {y} + {z} = 1")
+    P1.set_statement_lhs("(1:ℝ)/{x} + (1:ℝ)/{y} + (1:ℝ)/{z}")
     P1.set_statement_rhs("(9:ℝ)")
     P1.set_rhs_pos(True)
     
-    P = algebraic_op(P1,mode='sqrt_random')
+    P = algebraic_op(P1,mode='mod_var_idx', additional_arg=5)
+    print(P.variables)
+    print(P.condition)
+    print(P.statement_lhs)
+    print(P.statement_rhs)
     print(P.to_lean())
