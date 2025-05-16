e7ec24403a7375489ff696bab8afbc9da0a51361
haoyuzhao123 <thomaszhao1998@gmail.com>
2025-05-16 03:59:47 -0400

    clean code a bit

--- scripts_eval/inference_claude_icl_2gpu.sh
+++ scripts_eval/inference_claude_icl_2gpu.sh
@@ -4,21 +4,17 @@
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=24        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=10G         # memory per cpu-core (4G is default)
-#SBATCH --gres=gpu:2        # number of gpus per node
+#SBATCH --gres=gpu:2       # number of gpus per node
 #SBATCH --time=23:00:00          # total run time limit (HH:MM:SS)
-#SBATCH --mail-type=FAIL
-#SBATCH --mail-user=haoyu@princeton.edu
-#SBATCH --partition=pli-c
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=EMAIL
+#SBATCH --partition=PARTITION
 #SBATCH --output=slurm/%x-%j.out
 
-#### #SBATCH --qos=pli-cp
+#source /path/to/your/conda/etc/profile.d/conda.sh
+#conda activate CONDA_ENV
 
-source /scratch/gpfs/haoyu/miniconda3/etc/profile.d/conda.sh
-conda activate skillmixlean
-
-export LD_LIBRARY_PATH=/scratch/gpfs/haoyu/miniconda3/lib:$LD_LIBRARY_PATH
-
-export HF_HOME=/scratch/gpfs/haoyu/cache/
+#export LD_LIBRARY_PATH=/path/to/your/condalib:$LD_LIBRARY_PATH
 
 
 SPLIT=none
@@ -33,16 +29,13 @@ SEED=1
 MODEL_PATH="claude-3-7-sonnet-20250219"
 
 # choose split from amgm/cauchy/misc
+# choose type from seed/type1/type2
 PART=misc
-TYPE=1
-INPUT_PATH=/scratch/gpfs/haoyu/Deepseek/datasets/${PART}_test${TYPE}.jsonl
-ICL_FOLDER=/scratch/gpfs/haoyu/Deepseek/datasets/${PART}_valid
-#INPUT_PATH=/scratch/gpfs/haoyu/Deepseek/datasets/test.jsonl
-#OUTPUT_DIR=results/testAMGM_minif2ftestineq
+TYPE=seed
+INPUT_PATH=benchmark/${PART}_${TYPE}.jsonl
+ICL_FOLDER=benchmark/full_answer/${PART}_${TYPE}_4.9
 
-# for r1-distill-qwen
 OUTPUT_DIR=results/${PART}_test${TYPE}_icl_claude-3-7-sonnet
-#OUTPUT_DIR=results/test_qwq
 
 
 echo python scripts_eval/step1_inference_claude_icl.py --input_path ${INPUT_PATH}  --model_path ${MODEL_PATH}  --output_dir $OUTPUT_DIR --split $SPLIT --n $N --gpu 2 --seed $SEED --icl_folder $ICL_FOLDER
@@ -50,5 +43,4 @@ python scripts_eval/step1_inference_claude_icl.py --input_path ${INPUT_PATH}  --
 
 INPUT_FILE=${OUTPUT_DIR}/to_inference_codes.json
 
-#sbatch scripts_eval/compile_summarize.sh -i $INPUT_FILE -o $OUTPUT_DIR
-sbatch scripts_eval/compile_summarize_test_repl.sh -i $INPUT_FILE -o $OUTPUT_DIR
+sbatch scripts_eval/compile_summarize.sh -i $INPUT_FILE -o $OUTPUT_DIR
--- scripts_eval/inference_think_icl_2gpu.sh
+++ scripts_eval/inference_think_icl_2gpu.sh
@@ -4,46 +4,36 @@
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=24        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=10G         # memory per cpu-core (4G is default)
-#SBATCH --gres=gpu:2        # number of gpus per node
+#SBATCH --gres=gpu:2       # number of gpus per node
 #SBATCH --time=23:00:00          # total run time limit (HH:MM:SS)
-#SBATCH --mail-type=FAIL
-#SBATCH --mail-user=haoyu@princeton.edu
-#SBATCH --partition=pli-c
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=EMAIL
+#SBATCH --partition=PARTITION
 #SBATCH --output=slurm/%x-%j.out
 
-#### #SBATCH --qos=pli-cp
+#source /path/to/your/conda/etc/profile.d/conda.sh
+#conda activate CONDA_ENV
 
-source /scratch/gpfs/haoyu/miniconda3/etc/profile.d/conda.sh
-conda activate skillmixlean
-
-export LD_LIBRARY_PATH=/scratch/gpfs/haoyu/miniconda3/lib:$LD_LIBRARY_PATH
-
-export HF_HOME=/scratch/gpfs/haoyu/cache/
+#export LD_LIBRARY_PATH=/path/to/your/condalib:$LD_LIBRARY_PATH
 
 
-N=128
-SEED=1
 SPLIT=none
+N=16
 
-# R1-distill-qwen-32b
-#MODEL_PATH="/scratch/gpfs/haoyu/models/deepseek-r1-distill-qwen-32b/"
+SEED=1
 
 # Qwen2.5-Coder-Instruct
 MODEL_PATH="/scratch/gpfs/ARORA/haoyu/Qwen2.5-Coder-32B-Instruct"
 
 
 # choose split from amgm/cauchy/misc
+# choose type from seed/type1/type2
 PART=misc
-TYPE=1
-INPUT_PATH=/scratch/gpfs/haoyu/Deepseek/datasets/${PART}_test${TYPE}.jsonl
-ICL_FOLDER=/scratch/gpfs/haoyu/Deepseek/datasets/${PART}_valid
-#INPUT_PATH=/scratch/gpfs/haoyu/Deepseek/datasets/test.jsonl
-#OUTPUT_DIR=results/testAMGM_minif2ftestineq
+TYPE=seed
+INPUT_PATH=benchmark/${PART}_${TYPE}.jsonl
+ICL_FOLDER=benchmark/full_answer/${PART}_${TYPE}_4.9
 
-# for r1-distill-qwen
-#OUTPUT_DIR=results/${PART}_test${TYPE}_icl_think_r1-distill-qwen
-# for qwen coder
-OUTPUT_DIR=results/${PART}_test${TYPE}_icl_chat_qwen-coder
+OUTPUT_DIR=results/${PART}_test${TYPE}-icl_chat-qwen-coder
 
 
 echo python scripts_eval/step1_inference_think_icl.py --input_path ${INPUT_PATH}  --model_path ${MODEL_PATH}  --output_dir $OUTPUT_DIR --split $SPLIT --n $N --gpu 2 --seed $SEED --icl_folder $ICL_FOLDER
@@ -51,5 +41,4 @@ python scripts_eval/step1_inference_think_icl.py --input_path ${INPUT_PATH}  --m
 
 INPUT_FILE=${OUTPUT_DIR}/to_inference_codes.json
 
-#sbatch scripts_eval/compile_summarize.sh -i $INPUT_FILE -o $OUTPUT_DIR
-sbatch scripts_eval/compile_summarize_test_repl.sh -i $INPUT_FILE -o $OUTPUT_DIR
+sbatch scripts_eval/compile_summarize.sh -i $INPUT_FILE -o $OUTPUT_DIR
